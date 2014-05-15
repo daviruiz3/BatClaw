feb96cc238177a4d2001e1761aabc7aff4050d57
daviruiz3 <daviruiz3@gmail.com>
2014-05-15 15:15:05 -0700

    Added some more claw features

--- batclaw.js
+++ batclaw.js
@@ -1,6 +1,8 @@
 console.log("Hello World");
 
-require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+var os = require('os');
+
+require('dns').lookup(os.hostname(), function (err, add, fam) {
   console.log('addr: '+add);
 })
 
@@ -42,54 +44,15 @@ getLocalIPs = function () {
     console.log(localIPInfo);
 };
 
-/* http://jsfiddle.net/ThinkingStiff/z8KXd/ */
-function checkUploadSpeed( iterations, update ) {
-    var average = 0,
-        index = 0,
-        timer = window.setInterval( check, 5000 ); //check every 5 seconds
-    check();
-    
-    function check() {
-        var xhr = new XMLHttpRequest(),
-            url = '?cache=' + Math.floor( Math.random() * 10000 ), //prevent url cache
-            data = getRandomString( 1 ), //1 meg POST size handled by all servers
-            startTime,
-            speed = 0;
-        xhr.onreadystatechange = function ( event ) {
-            if( xhr.readyState == 4 ) {
-                speed = Math.round( 1024 / ( ( new Date() - startTime ) / 1000 ) );
-                average == 0 
-                    ? average = speed 
-                    : average = Math.round( ( average + speed ) / 2 );
-                update( speed, average );
-                index++;
-                if( index == iterations ) {
-                    window.clearInterval( timer );
-                };
-            };
-        };
-        xhr.open( 'POST', url, true );
-        startTime = new Date();
-        xhr.send( data );
-    };
-    
-    function getRandomString( sizeInMb ) {
-        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+`-=[]\{}|;':,./<>?", //random data prevents gzip effect
-            iterations = sizeInMb * 1024 * 1024, //get byte count
-            result = '';
-        for( var index = 0; index < iterations; index++ ) {
-            result += chars.charAt( Math.floor( Math.random() * chars.length ) );
-        };     
-        return result;
-    };
-};
-
-checkUploadSpeed( 30, function ( speed, average ) {
-    //document.getElementById( 'speed' ).textContent = 'speed: ' + speed + 'kbs';
-    //document.getElementById( 'average' ).textContent = 'average: ' + average + 'kbs';
-
-    console.log('speed: ' + speed + 'kbs');
-    console.log('average: ' + average + 'kbs');
-} );
+/* 
+Returns an array of objects containing information about each 
+CPU/core installed: model, speed (in MHz), and times 
+(an object containing the number of milliseconds the 
+CPU/core spent in: user, nice, sys, idle, and irq). 
+*/
+console.log(os.cpus());
+/* Returns system uptime in seconds */
+console.log(os.uptime());
+console.log(os.hostname());
 
 getLocalIPs();
